16e34518eea89724ab0e6de2b11a67c09640c6ce
Jeremia <jeripane@gmail.com>
2024-05-01 14:04:12 +0700

    create server config

--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const { PORT = 8080 } = process.env;
+
+const fs = require("fs");
+const path = require("path");
+const PUBLIC_DIRECTORY = path.join(__dirname, "../public");
+
+function getHTML(htmlFileName) {
+    const htmlFilePath = path.join(PUBLIC_DIRECTORY, htmlFileName);
+    return fs.readFileSync(htmlFilePath, "utf-8");
+}
+function onRequest(req, res) {
+    switch (req.url) {
+        case "/":
+            res.writeHead(200);
+            res.end(getHTML("index.html"));
+            return;
+        case "/cars":
+            res.writeHead(200);
+            res.end(getHTML("cariMobil.html"));
+            return;
+        default:
+            res.writeHead(404);
+            return;
+    }
+}
+
+const server = http.createServer(onRequest);
+server.listen(PORT, () => {
+    console.log("server sedang berjalan", PORT);
+});
